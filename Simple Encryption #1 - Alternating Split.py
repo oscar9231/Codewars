3421b0a7a62f0117f5415b43146dd428cc7df116
Oscar Li <oslee.cn@gmail.com>
2018-04-18 16:09:02 +0900

    Update Simple Encryption #1 - Alternating Split.py

--- Simple Encryption #1 - Alternating Split.py
+++ Simple Encryption #1 - Alternating Split.py
@@ -1,3 +1,21 @@
+#For building the encrypted string:
+#Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+#Do this n times!
+
+#Examples:
+
+#"This is a test!", 1 -> "hsi  etTi sats!"
+#"This is a test!", 2 -> "hsi  etTi sats!" -> "s eT ashi tist!"
+
+#Write two methods:
+
+#def encrypt(text, n)
+#def decrypt(encrypted_text, n)
+
+#For both methods:
+#If the input-string is null or empty return exactly this value!
+#If n is <= 0 then return the input text.
+
 def encrypt(text, n):
     if n <= 0:
         return text
