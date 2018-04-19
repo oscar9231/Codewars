90aa147a31c48f6ea0b92b88f3a039d4669a58ef
Oscar Li <oslee.cn@gmail.com>
2018-04-19 15:02:45 +0900

    Update Dubstep.py

--- Dubstep.py
+++ Dubstep.py
@@ -18,9 +18,5 @@
 
 
 def song_decoder(song):
-    song_name = ''
-    name_list = song.split('WUB')
-    for char in name_list:
-        if char != '':
-            song_name = song_name + char + ' '
-    return song_name[:-1]
+    name_list = song.replace('WUB', ' ').split()
+    return " ".join(name_list)
