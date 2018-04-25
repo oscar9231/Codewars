13ac01c52888ad8d58f3fda28d4073c1568bd5c3
Oscar Li <oslee.cn@gmail.com>
2018-04-25 15:00:04 +0900

    Update Convert string to camel case.py

--- Convert string to camel case.py
+++ Convert string to camel case.py
@@ -1,3 +1,14 @@
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+
+# Examples:
+
+# # returns "theStealthWarrior"
+# to_camel_case("the-stealth-warrior") 
+
+# # returns "TheStealthWarrior"
+# to_camel_case("The_Stealth_Warrior")
+
+
 def to_camel_case(text):
     if text == '':
         return ''
